7130a57cbf4495ac05f3f434f4d7dc80f296297a
Devin Riegle <riegledevin@gmail.com>
2021-01-11 22:36:58 -0500

    Remove generated code

--- lib/generated/client_index.dart
+++
@@ -1,1 +0,0 @@
-export 'ynab.swagger.dart' show Ynab;
--- lib/generated/client_mapping.dart
+++
@@ -1,5 +0,0 @@
-import 'ynab.swagger.dart';
-
-final Map<Type, Object Function(Map<String, dynamic>)> generatedMapping = {
-  ...YnabJsonDecoderMappings,
-};
